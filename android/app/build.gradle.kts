90232cb1fd8f00372adfe3d7f5c106f51ccd31fe
Yomna Salah <syomna444@gmail.com>
2025-07-23 23:34:03 +0300

    change android mode

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,8 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-             signingConfig = signingConfigs.getByName("release")
+            // signingConfig = signingConfigs.getByName("debug")
+            signingConfig signingConfigs.release
         }
     }
 }
