ad6fc6d055a2c4fc7cfea713ad97cf7042bc60cf
Yomna Salah <syomna444@gmail.com>
2026-07-22 22:08:23 +0300

    Update android target version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
     defaultConfig {
         applicationId = "com.yomna.azkar_app"
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 35
         versionCode = appVersionCode
         versionName = flutter.versionName
     }
